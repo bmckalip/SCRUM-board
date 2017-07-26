fd5fb3e4eed752a443590018265fe06e5005757f
Brian McKalip <brmckalip@gmail.com>
2017-07-26 11:11:38 -0400

    blah

--- SOTE/src/main/java/com/revature/util/Logger.java
+++ SOTE/src/main/java/com/revature/util/Logger.java
@@ -2,10 +2,12 @@ package com.revature.util;
 
 public class Logger implements Runnable{
 
+	private static final boolean logToDB = true;
+	private static final boolean logToConsole = true;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
