a94f0f35389c10f403d0ad02eb33499269ed5aec
Brian McKalip <brmckalip@gmail.com>
2017-08-01 14:15:06 -0400

    fixed some annotations

--- SOTE/src/main/java/com/revature/controller/BoardController.java
+++ SOTE/src/main/java/com/revature/controller/BoardController.java
@@ -1,10 +1,9 @@
 package com.revature.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
@@ -12,19 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RequestMapping("/board")
-@ResponseBody
-@RestController
+@Controller
 public class BoardController {
-	
 	@RequestMapping(value="/new", method=RequestMethod.POST)
 	public void newBoard(@RequestParam("user_id") int user_id, @RequestParam("title") String title){
 		//TODO: call service method to create board
-		
 	}
 	
 	@RequestMapping(value="/delete", method=RequestMethod.DELETE)
 	public void deleteBoard(@RequestParam("id") int id){
 		//TODO: call service method to delete board
 	}
-	
 }
