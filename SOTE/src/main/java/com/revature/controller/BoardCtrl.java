42f568b9d79b69f9f8d43dab455a2756652a1aeb
Robert D. Tuck <robertdill.tuck@gmail.com>
2017-08-05 16:24:48 -0400

    updated board controller to allow creation of a new board

--- SOTE/src/main/java/com/revature/controller/BoardCtrl.java
+++ SOTE/src/main/java/com/revature/controller/BoardCtrl.java
@@ -1,9 +1,13 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.pojo.Board;
@@ -22,12 +26,14 @@ public class BoardCtrl {
 	
 	@Autowired
 	AppService service;
-//	
-//	@RequestMapping(value="/new", method=RequestMethod.POST)
-//	public ResponseEntity<Board> newBoard(@RequestParam("user_id") int user_id, @RequestParam("title") String title){
-//		//TODO: call service method to create board
-//		return new ResponseEntity<Board>(HttpStatus.OK);
-//	}
+	
+	@RequestMapping(value="/new", method=RequestMethod.POST)
+	public ResponseEntity<Board> newBoard(@RequestBody Board board){
+		System.out.println("hitting /board/new - POST");
+		service.saveBoard(board);
+	
+		return new ResponseEntity<Board>(HttpStatus.OK);
+	}
 //	
 //	@RequestMapping(value="/delete", method=RequestMethod.DELETE)
 //	public ResponseEntity<Board> deleteBoard(@RequestParam("id") int id){
