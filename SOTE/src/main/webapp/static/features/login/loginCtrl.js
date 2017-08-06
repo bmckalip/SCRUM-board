9c3ca1ce42df6f81a513f3dfdc35095ce9915a63
Adedayo Salam <adeosalam@gmail.com>
2017-08-06 14:27:51 -0400

    so far

--- SOTE/src/main/webapp/static/features/login/loginCtrl.js
+++ SOTE/src/main/webapp/static/features/login/loginCtrl.js
@@ -6,14 +6,30 @@
  */
 
 
-	app.controller('loginCtrl', function($scope, $http, $rootscope){
+	app.controller('loginCtrl', function($scope, $http, $rootScope){
 		$scope.data = {
 				email : "",
 				password : ""
 		};
-		
 		$scope.loginSubmit = function(){
+			console.log('testing')
+
+//			var url="http://localhost:8085/SOTE/rest/login";
+//			var user= 
+//			{
+//				    "userId": null,
+//				    "userEmail": $scope.data.email,
+//				    "userPassword": $scope.data.password,
+//				    "permission": null,
+//				    "userFirstName": null,
+//				    "userLastName": null
+//				}
+//			var parameter = JSON.stringify(user);
+//				$http.post(url, parameter).
+//				success(function(data, status, headers, config) { //valid
+//					console.log(data);
+//				}). error(function(data, status, headers, config) {invalid });	
 
 		}
-	})	
+	})	;
 
